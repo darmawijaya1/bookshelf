88dfefac4f8b77ba0c3560dd2a3ad490b79ae3f7
darmawijaya1 <116532006+darmawijaya1@users.noreply.github.com>
2025-06-19 15:14:18 +0700

    okk

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 9000, // mendukung Railway & lokal
-    host: '0.0.0.0',
+    port: 9000,
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
@@ -13,6 +13,7 @@ const init = async () => {
   });
 
   server.route(routes);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
